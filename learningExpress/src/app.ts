459619a0d80a3507bc23a22f4719e22042fcfed1
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-21 09:10:54 +0400

    Understanding express middleware

--- learningExpress/src/app.ts
+++ learningExpress/src/app.ts
@@ -6,6 +6,7 @@ import express, {
 import { userRoute } from './modules/user/user.route';
 import { profileRoute } from './modules/profile/profile.route';
 import { authRoute } from './modules/auth/auth.route';
+import fs from 'fs';
 
 const app: Application = express();
 
@@ -13,6 +14,18 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log('Method - URL - Time:', req.method, req.url, Date.now());
+
+  const log = `\nMethod -> ${req.method} - Time -> ${Date.now()} - URL -> ${req.url}\n`;
+
+  fs.appendFile('logger.txt', log, err => {
+    console.log(err);
+  });
+
+  next();
+});
 
 // GET
 app.get('/', (req: Request, res: Response) => {

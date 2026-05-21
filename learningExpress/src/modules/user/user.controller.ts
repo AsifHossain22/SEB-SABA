93a2c930884337135116c93bab50bfa40de75421
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-21 18:11:50 +0400

    Explored 'SendResponse Utility Creation

--- learningExpress/src/modules/user/user.controller.ts
+++ learningExpress/src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import { userService } from './user.service';
+import sendResponse from '../../utils/sendResponse';
 
 // CreateUser - POST
 const createUser = async (req: Request, res: Response) => {
@@ -9,13 +10,15 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const result = await userService.createUserIntoDB(req.body);
 
-    res.status(201).json({
+    sendResponse(res, {
+      statusCode: 201,
       success: true,
       message: 'User created successfully!',
       data: result.rows[0],
     });
   } catch (error: any) {
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: error.message,
       error: error,

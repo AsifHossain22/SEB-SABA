7aeb26ccbbc011ed260b4b7df80cb4f4053e0e5b
AsifHossain22 <only1asif4you22@gmail.com>
2026-06-08 17:52:17 +0400

    LIMIT, OFFSET Pagination

---
+++ MC/Mission 3 - Be A RDBMS Rockstar/M3M15/LIMIT, OFFSET Pagination.sql
@@ -0,0 +1,19 @@
+-- Limit
+select * from students limit 5;
+
+-- Offset
+select * from students limit 5 offset 5;
+
+-- Pagination 
+select * from students limit 5 offset 5 * 0;
+select * from students limit 5 offset 5 * 1;
+select * from students limit 5 offset 5 * 2;
+
+
+
+
+
+
+
+
+

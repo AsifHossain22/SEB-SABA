a4fc8c6ce475f264d960749b7e5eb66ff71efc4e
AsifHossain22 <only1asif4you22@gmail.com>
2026-06-23 23:38:08 +0400

    Express setup and env file

--- Project Prisma Press/project-prisma-press-backend/src/app.ts
+++ Project Prisma Press/project-prisma-press-backend/src/app.ts
@@ -1,6 +1,22 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import config from './config';
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors({}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi, Prisma Server!');

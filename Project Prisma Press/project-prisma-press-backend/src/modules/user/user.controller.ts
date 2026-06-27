d05e0a96f4d4ca4c45edd28b5536b1fba37d7ba3
AsifHossain22 <only1asif4you22@gmail.com>
2026-06-28 00:55:07 +0400

    Auth Middleware For Role Based Access Control

--- Project Prisma Press/project-prisma-press-backend/src/modules/user/user.controller.ts
+++ Project Prisma Press/project-prisma-press-backend/src/modules/user/user.controller.ts
@@ -3,7 +3,6 @@ import httpStatus from 'http-status';
 import { userService } from './user.service';
 import { catchAsync } from '../../utils/catchAsync';
 import { sendResponse } from '../../utils/sendResponse';
-import jwt from 'jsonwebtoken';
 import config from '../../config';
 import { jwtUtils } from '../../utils/jwt';
 
@@ -56,19 +55,21 @@ const registerUser = catchAsync(async (req: Request, res: Response) => {
 // GetUserProfile
 const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { accessToken } = req.cookies;
-    console.log(accessToken);
+    // const { accessToken } = req.cookies;
+    // console.log(req.user, 'User request');
 
-    const verifiedToken = jwtUtils.verifyToken(
-      accessToken,
-      config.jwt_access_secret,
+    // const verifiedToken = jwtUtils.verifyToken(
+    //   accessToken,
+    //   config.jwt_access_secret,
+    // );
+
+    // if (typeof verifiedToken === 'string') {
+    //   throw new Error(verifiedToken);
+    // }
+
+    const profile = await userService.getMyProfileFromDB(
+      req.user?.id as string,
     );
-
-    if (typeof verifiedToken === 'string') {
-      throw new Error(verifiedToken);
-    }
-
-    const profile = await userService.getMyProfileFromDB(verifiedToken.id);
 
     sendResponse(res, {
       success: true,
--- Project Prisma Press/project-prisma-press-backend/src/modules/user/user.route.ts
+++ Project Prisma Press/project-prisma-press-backend/src/modules/user/user.route.ts
@@ -1,12 +1,71 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { userController } from './user.controller';
+import { jwtUtils } from '../../utils/jwt';
+import config from '../../config';
+import { Role } from '../../../generated/prisma/enums';
+import httpStatus from 'http-status';
 
 const router = Router();
+
+// GlobalDeclaration
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        name: string;
+        email: string;
+        role: Role;
+      };
+    }
+  }
+}
 
 // UserRegister
 router.post('/register', userController.registerUser);
 
 // UserProfile
-router.get('/me', userController.getMyProfile);
+router.get(
+  '/me',
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.cookies);
+
+    const { accessToken } = req.cookies;
+    console.log(accessToken);
+
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    );
+
+    if (typeof verifiedToken === 'string') {
+      throw new Error(verifiedToken);
+    }
+
+    const { email, name, id, role } = verifiedToken;
+
+    // const requiredRoles = ["ADMIN", "USER", "AUTHOR"];
+    const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+    if (!requiredRoles.includes(role)) {
+      return res.status(403).json({
+        success: false,
+        statusCode: httpStatus.FORBIDDEN,
+        message:
+          "Forbidden. You don't have permission to access this resource!",
+      });
+    }
+
+    req.user = {
+      id,
+      name,
+      email,
+      role,
+    };
+
+    next();
+  },
+  userController.getMyProfile,
+);
 
 export const userRoutes = router;

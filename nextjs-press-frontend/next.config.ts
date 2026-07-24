1433dd888781dc39454ba933f40f0ff7aa1e8f32
AsifHossain22 <only1asif4you22@gmail.com>
2026-07-24 09:50:23 +0400

    Implemented Subscribe Functionalities In Payment Page

--- nextjs-press-frontend/next.config.ts
+++ nextjs-press-frontend/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'static.vecteezy.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
